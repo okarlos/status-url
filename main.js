7f99db25acfcf3eaef871fffc056202ab5826d07
Karlos Kliemann <108680411+okarlos@users.noreply.github.com>
2022-10-18 10:42:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const buttonTest = document.getElementById("testar");
+const buttonClear = document.getElementById("limpar")
+const lista = document.querySelectorAll(".url");
+
+buttonTest.addEventListener("click", () => {
+
+    for (let i = 0; i < lista.length; i++) {
+        const status = conectaURL(lista[i].innerHTML + '/help',i);
+        const pinta = document.querySelector(`[contador="${i}"]`);
+    }
+
+})
+
+buttonClear.addEventListener("click", () => {
+
+    for (let i = 0; i < lista.length; i++) {
+        lista[i].classList.remove("ok");
+        lista[i].classList.remove("erro");
+    }
+    
+})
+
+function conectaURL(URL, i) {
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", URL);
+
+    xhr.addEventListener("load", function() {
+        console.log(URL +" - "+ xhr.status) 
+
+        if (xhr.status === 200) {
+            lista[i].classList.add("ok")
+        }else {
+            lista[i].classList.add("erro")
+        }
+
+    })
+
+    xhr.send();
+}
